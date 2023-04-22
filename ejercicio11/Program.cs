3a65c05f48a93d925cb4ae4ebf204b8af65fc300
Maydau1524 <126518754+Maydau1523@users.noreply.github.com>
2023-04-22 14:24:02 -0600

    Delete ejercicio11 directory

--- ejercicio11/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿double radio=10;
-double circunferencia = calcularcircunferencia(radio);
-Console.WriteLine($"La circunferencia de un círculo con radio {radio} es: {circunferencia}");
-static double calcularcircunferencia(double radio)
-{
-    double circunferencia = 2 * Math.PI * radio;
-    return circunferencia;
-}
