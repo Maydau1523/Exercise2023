156a75cf99860fe2cd1f8868dcdb44c22a52c84d
maydau1524 <21040107@itszo.edu.mx>
2023-04-22 14:46:00 -0600

    agreaar session7

---
+++ Session7/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.Write("Segunda linea Sin Salto");
+
+for(int i = 0; i<=10; i++)
+
+    Console.WriteLine(i);
+
+
+
+int numeroPersonas = 11;
+
+string NombrePersona = "Daniela Favela";
+
+Console.WriteLine( "En este room hay {0} personas y una se llama {1}", numeroPersonas, NombrePersona);
+
+
