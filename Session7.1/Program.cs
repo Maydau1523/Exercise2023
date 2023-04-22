1823aae81f0b70c1931efd68df224504f751522d
maydau1524 <21040107@itszo.edu.mx>
2023-04-22 14:52:58 -0600

    se agrego session 7.1 y session 7.2

---
+++ Session7.1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escribe un numeros impares");
+
+class NumerosImpares
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Imprimir numeros impares del 1 al 99");
+
+        for (int i = 1; i < 100; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
+
+    
+
+
+
+
+
