c6027cfb74f8e1912c62a9f113462b659382a721
maydau1524 <21040107@itszo.edu.mx>
2023-05-07 19:00:33 -0600

    mi nuevo ejercicio

---
+++ Session19/ejercise5/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Fighter fighter = new Fighter();
+        fighter.Nombre = "John Doe";
+        fighter.Peso = 150;
+
+        Console.WriteLine("Nombre del luchador: " + fighter.Nombre);
+        Console.WriteLine("Peso del luchador: " + fighter.Peso + " lbs");
+        Console.WriteLine("Categoría del luchador: " + fighter.Categoria);
+    }
+}
