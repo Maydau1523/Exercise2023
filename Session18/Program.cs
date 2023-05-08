df63a369ecd2e22149f189894ac536c885a1a4ba
maydau1524 <21040107@itszo.edu.mx>
2023-05-07 18:04:54 -0600

    mi segundo ejericio

---
+++ Session18/Program.cs
@@ -0,0 +1,26 @@
+﻿{
+bool es_curp(const char* cadena) 
+    return (strlen(cadena) == 18);
+}
+
+bool es_rfc(const char* cadena) {
+    int longitud = strlen(cadena);
+    return (longitud == 10 || longitud == 13);
+}
+
+int main() {
+    char cadena[50];
+
+    printf("Ingrese una cadena: ");
+    scanf("%s", cadena);
+
+    if (es_curp(cadena)) {
+        printf("Es un CURP.\n");
+    } else if (es_rfc(cadena)) {
+        printf("Es un RFC.\n");
+    } else {
+        printf("No es ni un CURP ni un RFC.\n");
+    }
+
+    return 0;
+}
