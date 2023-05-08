5b677b4556ea9d07859aa0f8ff9c83b865e2ac5d
maydau1524 <21040107@itszo.edu.mx>
2023-05-07 18:49:04 -0600

    mi tercer ejercicio

---
+++ Session19/ejercise3/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+using System;
+
+class Program
+{
+    struct Libro
+    {
+        public string titulo;
+        public string autor;
+    }
+
+    static void Main(string[] args)
+    {
+        Libro libro1, libro2;
+
+        Console.WriteLine("Información del libro 1:");
+        Console.Write("Ingrese el nombre del libro: ");
+        libro1.titulo = Console.ReadLine();
+        Console.Write("Introduzca el autor: ");
+        libro1.autor = Console.ReadLine();
+
+        Console.WriteLine("Información del libro 2:");
+        Console.Write("Ingrese el nombre del libro: ");
+        libro2.titulo = Console.ReadLine();
+        Console.Write("Introduzca el autor: ");
+        libro2.autor = Console.ReadLine();
+
+        Console.WriteLine("Rendimiento esperado:");
+        Console.WriteLine("1: Título = {0}, Autor = {1}", libro1.titulo, libro1.autor);
+        Console.WriteLine("2: Título = {0}, Autor = {1}", libro2.titulo, libro2.autor);
+    }
+}
