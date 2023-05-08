d8b9c7cac6fd55def0a4f52273a96bc5cfd329f0
maydau1524 <21040107@itszo.edu.mx>
2023-05-07 18:17:04 -0600

    primer ejemplo

---
+++ Session19/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+class Program
+{
+    static void Main()
+    {
+        short x, y;
+
+        Console.Write("Ingrese las coordenadas x e y del punto: ");
+        string[] coordenadas = Console.ReadLine().Split(' ');
+
+        x = Convert.ToInt16(coordenadas[0]);
+        y = Convert.ToInt16(coordenadas[1]);
+
+        Console.WriteLine("Las coordenadas del punto son: ({0}, {1})", x, y);
+    }
+}
+
+
+
