fe8b4ee7c232d9b67f10dd820cf5d79fcba5d84f
maydau1524 <21040107@itszo.edu.mx>
2023-05-07 18:54:47 -0600

    mi cuarto ejericio

---
+++ Session19/ejercise4/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+using System;
+
+struct Cuadrado
+{
+    public int largo;
+    public int ancho;
+}
+
+class Program
+{
+    static Cuadrado ObtenerCuadrado()
+    {
+        Cuadrado cuadrado;
+
+        Console.WriteLine("Ingrese las dimensiones del Cuadrado (igual largo y ancho):");
+        Console.Write("Largo: ");
+        cuadrado.largo = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Ancho: ");
+        cuadrado.ancho = Convert.ToInt32(Console.ReadLine());
+
+        return cuadrado;
+    }
+
+    static void Main(string[] args)
+    {
+        Cuadrado miCuadrado = ObtenerCuadrado();
+
+        int perimetro = (miCuadrado.largo + miCuadrado.ancho) * 2;
+        int area = miCuadrado.largo * miCuadrado.ancho;
+
+        Console.WriteLine("Perímetro y Área del cuadrado:");
+        Console.WriteLine("Largo: {0} Ancho: {1}", miCuadrado.largo, miCuadrado.ancho);
+        Console.WriteLine("Perímetro: {0} Área: {1}", perimetro, area);
+    }
+}
+
